fc387d8d89c341057f7d45542e9ed713767ba531
mareme <mariamathiaw92@gmail.com>
2024-02-28 12:30:41 +0000

    Création du fichier index.php qui représente la page d'accueil de l'application

--- config.php
+++ config.php
@@ -9,13 +9,13 @@ $nom_db = 'Projet_ITS';
 
 try {
     // Connexion à la base de données avec PDO
-    $connexion = new PDO("mysql:host=$serveur_db;dbname=$nom_db", $utilisateur_db, $mot_de_passe_db);
+    $connexion = new mysqli($serveur_db, $utilisateur_db, $mot_de_passe_db,$nom_db);
 
     // Paramétrage de la connexion pour afficher les erreurs en cas de problème
-    $connexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    //$connexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
     echo "Connexion à la base de données réussie.";
-} catch (PDOException $e) {
+} catch (mysqliException $e) {
     die("Erreur de connexion à la base de données : " . $e->getMessage());
 }
 
